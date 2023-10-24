e77ce7febe5d63331b08ff4a43a925223a553bdf
HakanKara75 <hakanbatirhan@gmail.com>
2023-10-24 19:26:43 +0300

    v

--- cypress/e2e/stepDefinitions/ReadAndWriteFile.js
+++ cypress/e2e/stepDefinitions/ReadAndWriteFile.js
@@ -8,16 +8,36 @@ import { example } from "../../fixtures/example";
            
         })
     })
+   
 Given('visit the saucedemo', () => {
     cy.visit("https://saucedemo.com")
+     // Burada 'text' değişkeni içeriği içerir
+    cy.get('.login_credentials_wrap-inner').invoke('text').then((text) => {
+       
+        // İçeriği bir dosyaya yazmak için cy.writeFile kullanabilirsiniz.
+        cy.writeFile("saucedemoNotlari.txt", text);
+      });
 })
 
 When('type username and password and click the login button', () => {
      
         cy.login(login.username, login.password)
-   })
 
+        cy.get('[data-test="error"]').invoke('text').then((text) => {
+       
+            // İçeriği bir dosyaya yazmak için cy.writeFile kullanabilirsiniz.
+            cy.writeFile("saucedemoNotlari.txt", text);
+   })
+})
 
 Then('assert the page', () => {
     cy.get('[data-test="error"]').should('be.visible')
+
+    //dosyayi okutup icinde bir kelimeyi dogrulamak icin
+    cy.readFile("saucedemoNotlari.txt").then((text) => {
+        expect(text).to.contain("user")
+    });
+
+    cy.readFile("saucedemoNotlari.txt").should('contain', 'password')
+
 })
