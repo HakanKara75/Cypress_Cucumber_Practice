ec8458b82dbf47d31feb9b52ecfa32466676dc6a
HakanKara75 <hakanbatirhan@gmail.com>
2023-10-28 22:53:45 +0300

    v

---
+++ cypress/e2e/scenarioSteps/wikipedia.feature
@@ -0,0 +1,15 @@
+Feature: Search for an article on Wikipedia
+
+Scenario: We should find one article and land on its page
+
+Given I open web browser for "https://www.wikipedia.org/"
+
+When I type "Nicola Tesla" on the search box And I click on search button
+
+Then the URL should be "https://en.wikipedia.org/wiki/Nikola_Tesla"
+
+And I want to see an image under title "Nikola Tesla" on the right column
+
+And I want to read information about "Born", "Died", "Education" on the right column
+
+And I want to read a section about "Early Years"
---
+++ cypress/e2e/stepDefinitions/wikipedia.js
@@ -0,0 +1,33 @@
+import { Given, When, Then, And, Scenario } from "cypress-cucumber-preprocessor/steps";
+
+Given('I open web browser for {string}', (url) => {
+    cy.visit(url)
+})
+
+When ('I type {string} on the search box And I click on search button', (searchText)=>{
+    cy.get('#searchInput').type(searchText);
+    cy.get('.pure-button').click()
+ })
+ 
+ Then('the URL should be {string}',(urlText)=>{
+
+
+    cy.url().should('include', urlText); 
+})
+
+And('I want to see an image under title {string} on the right column',()=>{
+    cy.get('.infobox-image > .mw-default-size > .mw-file-description > .mw-file-element') .should('be.visible');
+
+})
+
+And('I want to read information about {string} {string} {string}on the right column',(born, died, education)=>{
+    cy.get('.biography').should('include', born)
+    cy.get('.biography').should('include', died)
+    cy.get('.biography').should('include', education)
+
+})
+
+And('I want to read a section about {string}',(year)=>{
+
+    cy.get('#Early_years').should('include', year)
+})
